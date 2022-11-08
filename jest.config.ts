8a91c3d01f8baa808412348e7444fa683e744087
weton01 <wellington_ferreira280@hotmail.com>
2022-11-08 13:29:06 -0300

    chore: add jest configs and scripts

--- jest.config.ts
+++ jest.config.ts
@@ -1,6 +1,13 @@
 export default {
   roots: ['<rootDir>/src'],
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/main/**',
+    '!<rootDir>/src/**/*-protocols.ts',
+    '!**/protocols/**',
+    '!<rootDir>/src/**/index.ts',
+    '!**/test/**'
+  ],
   clearMocks: true,
   transform: {
     '.+\\.ts$': 'ts-jest'
