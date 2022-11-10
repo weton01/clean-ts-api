3d44b53921fad6015dbe108b1d3109182b3766f8
weton01 <wellington_ferreira280@hotmail.com>
2022-11-10 14:28:54 -0300

    refactor: ensure MongoHelper reconnect

---
+++ src/infra/db/mongodb/helpers/mongo-helper.spec.ts
@@ -0,0 +1,18 @@
+import { MongoHelper as sut } from './mongo-helper'
+
+describe('Mongo Helper', () => {
+  beforeAll(async () => {
+    await sut.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await sut.disconnect()
+  })
+  test('Should reconnect if mongodb in down', async () => {
+    let accountCollection = sut.getCollection('accounts')
+    expect(accountCollection).toBeTruthy()
+    await sut.disconnect()
+    accountCollection = sut.getCollection('accounts')
+    expect(accountCollection).toBeTruthy()
+  })
+})
